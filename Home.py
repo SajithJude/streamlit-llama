50f679b0dc4dd63178e1af24ef3dcf9bfd7cbe63
Jude Sajith <judesajith.aj@gmail.com>
2023-06-20 18:06:24 +0530

    ssss

--- Home.py
+++ Home.py
@@ -31,11 +31,11 @@ def process_pdf(uploaded_file):
     llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.15, model_name="text-davinci-003", max_tokens=1000))
     service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
     
-    if "index" not in st.session_state:
-        index = GPTVectorStoreIndex.from_documents(documents,service_context=service_context)
-        retriever = index.as_retriever(retriever_mode='embedding')
-        index = RetrieverQueryEngine(retriever)
-        st.session_state.index = index
+    # if "index" not in st.session_state:
+    index = GPTVectorStoreIndex.from_documents(documents,service_context=service_context)
+    retriever = index.as_retriever(retriever_mode='embedding')
+    index = RetrieverQueryEngine(retriever)
+    st.session_state.index = index
     # st.session_state.index = index
     return st.session_state.index
 
